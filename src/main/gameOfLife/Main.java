6020db57a2e334ae00c69bf2ad636c9950662e8b
Nickirus <Nickirus@gmail.com>
2019-03-18 01:10:09 +0300

    changed closing process. Added speed regulation process by keywords

--- src/main/gameOfLife/Main.java
+++ src/main/gameOfLife/Main.java
@@ -6,11 +6,12 @@ import java.awt.*;
 import java.util.Arrays;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyAdapter;
 
 public class Main extends JFrame implements Runnable {
     private static boolean isOnGameFlg = true;
-    private static int WidthOfPlane = 60;
+    private static int widthOfPlane = 60;
     private static int planePresent[][] = new int[60][60];
     private static int planeFuture[][] = new int[60][60];
     private static int planeDead[][] = new int[60][60];
@@ -23,14 +24,15 @@ public class Main extends JFrame implements Runnable {
     private static int sumOfEight, center;
 
     private static int planeTopology(int i) {
-        if (i > (WidthOfPlane - 1)) i = i - WidthOfPlane;
-        if (i < 0) i = WidthOfPlane + i;
+        if (i > (widthOfPlane - 1)) i = i - widthOfPlane;
+        if (i < 0) i = widthOfPlane + i;
         return i;
     }
 
     private static void planeNavigation() {
         for (int i = 0; i < planePresent.length; i++) {
             for (int j = 0; j < planePresent[i].length; j++) {
+
                 center = planePresent[i][j];
                 i = i - 1;
                 i = planeTopology(i);
@@ -110,7 +112,7 @@ public class Main extends JFrame implements Runnable {
             }
     }
 
-    public static void setPlanePresent(int array[][]) {
+    private static void setPlanePresent(int array[][]) {
         changeCounter();
         for (int i = 0; i < planePresent.length; i++) {
             for (int j = 0; j < planePresent[i].length; j++) {
@@ -119,7 +121,7 @@ public class Main extends JFrame implements Runnable {
         }
     }
 
-    public static void setPlaneBirth(int array[][]) {
+    private static void setPlaneBirth(int array[][]) {
         for (int i = 0; i < planeBirth.length; i++) {
             for (int j = 0; j < planeBirth[i].length; j++) {
                 planeBirth[i][j] = array[i][j];
@@ -127,7 +129,7 @@ public class Main extends JFrame implements Runnable {
         }
     }
 
-    public static void setPlaneDead(int array[][]) {
+    private static void setPlaneDead(int array[][]) {
         for (int i = 0; i < planeDead.length; i++) {
             for (int j = 0; j < planeDead[i].length; j++) {
                 planeDead[i][j] = array[i][j];
@@ -137,45 +139,50 @@ public class Main extends JFrame implements Runnable {
 
     @Override
     public void paint(Graphics g) {
-        while (isOnGameFlg) {
-            int x = 10 + 4 + 1 + 5;
-            int y = 10 + 26 + 1 + 5;
-            super.paint(g);
 
-            Color colorLine = new Color(29, 29, 29);
-            g.setColor(colorLine);
-            for (; x < 675 + 5; x += 11) g.drawLine(x, 0, x, 700);
-            for (; y < 695 + 5; y += 11) g.drawLine(0, y, 680, y);
+        int x = 10 + 4 + 1 + 5;
+        int y = 10 + 26 + 1 + 5;
+        super.paint(g);
 
-            Color colorOval = new Color(40, 141, 113);
-            g.setColor(colorOval);
-            for (int i = 0; i < planePresent.length; i++) {
-                for (int j = 0; j < planePresent[i].length; j++) {
-                    if (planePresent[i][j] == 1)
-                        g.drawOval(11 * j + 4 + 5, 11 * i + 26 + 5, 11, 11);
-                }
+        Color colorLine = new Color(29, 29, 29);
+        g.setColor(colorLine);
+        for (; x < 675 + 5; x += 11) g.drawLine(x, 0, x, 700);
+        for (; y < 695 + 5; y += 11) g.drawLine(0, y, 680, y);
+
+        Color colorOval = new Color(40, 141, 113);
+        g.setColor(colorOval);
+        for (int i = 0; i < planePresent.length; i++) {
+            for (int j = 0; j < planePresent[i].length; j++) {
+                if (planePresent[i][j] == 1)
+                    g.drawOval(11 * j + 4 + 5, 11 * i + 26 + 5, 11, 11);
             }
+        }
 
-            Color colorDead = new Color(141, 92, 51);
-            g.setColor(colorDead);
-            for (int i = 0; i < planeDead.length; i++) {
-                for (int j = 0; j < planeDead[i].length; j++) {
-                    if (planeDead[i][j] == 1)
-                        g.drawOval(11 * j + 4 + 5, 11 * i + 26 + 5, 11, 11);
-                }
+        Color colorDead = new Color(141, 92, 51);
+        g.setColor(colorDead);
+        for (int i = 0; i < planeDead.length; i++) {
+            for (int j = 0; j < planeDead[i].length; j++) {
+                if (planeDead[i][j] == 1)
+                    g.drawOval(11 * j + 4 + 5, 11 * i + 26 + 5, 11, 11);
             }
-            run();
+        }
+        run();
 
-            Color colorBirth = new Color(39, 60, 50);
-            g.setColor(colorBirth);
-            for (int i = 0; i < planeBirth.length; i++) {
-                for (int j = 0; j < planeBirth[i].length; j++) {
-                    if (planeBirth[i][j] == 1)
-                        g.drawOval(11 * j + 4 + 5, 11 * i + 26 + 5, 11, 11);
-                }
+        Color colorBirth = new Color(39, 60, 50);
+        g.setColor(colorBirth);
+        for (int i = 0; i < planeBirth.length; i++) {
+            for (int j = 0; j < planeBirth[i].length; j++) {
+                if (planeBirth[i][j] == 1)
+                    g.drawOval(11 * j + 4 + 5, 11 * i + 26 + 5, 11, 11);
             }
+        }
+
+        if (isOnGameFlg) {
+            this.repaint();
             run();
         }
+
+
     }
 
     private static void messageGameOver() {
@@ -197,9 +204,21 @@ public class Main extends JFrame implements Runnable {
         getContentPane().setBackground(colorBack);
 
         addWindowListener(new WindowAdapter() {
+            @Override
             public void windowClosing(WindowEvent e) {
                 isOnGameFlg = false;
                 System.exit(0);
+            }
+        });
+        addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+                    speed += 10;
+                }
+                if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+                    if (speed > 40) speed -= 10;
+                }
             }
         });
     }
